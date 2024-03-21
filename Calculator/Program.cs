7e02a09cd163180e652b4d07605bc7736c9a5148
Jlucascaraujo <90482583+Jlucascaraujo@users.noreply.github.com>
2024-03-21 10:30:52 -0300

    Add files via upload
    Started this activity and went really far but realized I didn't know how to check the value of a variable and make the condition work for the exception of a wrong character being the input. So I went and googled, found the "var is int", "GetType()" which made me write the following code:
    
    using System.Threading.Channels;
    
    Console.WriteLine("Hello!");
    
    var firstInputMsg = "Input the first number: ";
    Console.WriteLine(firstInputMsg);
    var firstNum = Console.ReadLine();
    
    var secondInputMsg = "Input the second number: ";
    Console.WriteLine(secondInputMsg);
    var secondNum = Console.ReadLine();
    
    var options = "What do you want to do with those numbers?\n" + "[A]dd\n" + "[S]ubtract\n" + "[M]ultiply\n";
    Console.WriteLine(options);
    var userChoice = Console.ReadLine();
    
    if(userChoice is string)
    {
        if(userChoice == "A"||userChoice == "a")
        {
            if (firstNum == null||secondNum == null)
            {
                Console.WriteLine("Input a number!");
            } 
            else if(firstNum.GetType() == typeof(int)&&secondNum.GetType() == typeof(int))
            {
                Console.WriteLine(int.Parse(firstNum) + int.Parse(secondNum));
            }
            else
            {
                Console.WriteLine("Invalid input!");
            }
        }
        else if(userChoice == "S"||userChoice == "s")
        {
            if (firstNum == null||secondNum == null)
            {
                Console.WriteLine("Input a number!");
            }
            else if (firstNum.GetType() == typeof(int)&&secondNum.GetType() == typeof(int))
            {
                Console.WriteLine(int.Parse(firstNum) - int.Parse(secondNum));
            }
            else
            {
                Console.WriteLine("Invalid input!");
            }
        }
        else if(userChoice == "M"||userChoice == "m")
        {
            if (firstNum == null||secondNum == null)
            {
                Console.WriteLine("Input a number!");
            }
            else if (firstNum.GetType() == typeof(int)&&secondNum.GetType() == typeof(int))
            {
                Console.WriteLine(int.Parse(firstNum) * int.Parse(secondNum));
            }
            else
            {
                Console.WriteLine("Invalid input!");
            }
        }
        else
        {
            Console.WriteLine("What the hell happened!?");
        }
    } else
    {
        Console.WriteLine("Invalid Character!");
    }
    
    var closeMsg = "Press any key to close.";
    Console.WriteLine(closeMsg);
    Console.ReadKey();
    
    But I was getting an error where it always stated the input as invalid. I had to go to copilot to get the answers and it showed me the TryParse method of checking the variable, so the result of the copilot assisted code is the one here in GitHub, the calculator project.

---
+++ Calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Hello!");
+
+var firstInputMsg = "Input the first number: ";
+Console.WriteLine(firstInputMsg);
+var firstNum = Console.ReadLine();
+
+var secondInputMsg = "Input the second number: ";
+Console.WriteLine(secondInputMsg);
+var secondNum = Console.ReadLine();
+
+var options =   "What do you want to do with those numbers?\n" +
+                "[A]dd\n" +
+                "[S]ubtract\n" +
+                "[M]ultiply\n";
+
+Console.WriteLine(options);
+var userChoice = Console.ReadLine();
+
+if(userChoice is string)
+{
+    if(userChoice == "A"||userChoice == "a")
+    {
+        if(int.TryParse(firstNum, out int num1) && int.TryParse(secondNum, out int num2))
+        {
+            Console.WriteLine($"Result: {num1 + num2}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please, input a valid number!");
+        }
+    }
+    else if(userChoice == "S"||userChoice == "s")
+    {
+        if (int.TryParse(firstNum, out int num1) && int.TryParse(secondNum, out int num2))
+        {
+            Console.WriteLine($"Result: {num1 - num2}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please, input a valid number!");
+        }
+    }
+    else if(userChoice == "M"||userChoice == "m")
+    {
+        if (int.TryParse(firstNum, out int num1) && int.TryParse(secondNum, out int num2))
+        {
+            Console.WriteLine($"Result: {num1 * num2}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please, input a valid number!");
+        }
+    }
+    else
+    {
+        Console.WriteLine(value: "Invalid Choice! Please, input A, S or M.");
+    }
+} else
+{
+    Console.WriteLine("What the hell happened!?");
+}
+
+var closeMsg = "Press any key to close.";
+Console.WriteLine(closeMsg);
+Console.ReadKey();
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+var seeAll = "See all TODOs.";
+var addTodo = "Add a TODO.";
+var removeTodo = "Remove a TODO.";
+var exitStage = "Exit.";
+
+if ((userChoice == "S")||(userChoice == "s"))
+    {
+        MessageSelectedChoice(seeAll);
+    }
+else if ((userChoice == "A")||(userChoice == "a"))
+    {
+        MessageSelectedChoice(addTodo);
+    }
+else if ((userChoice == "R")||(userChoice == "r"))
+    {
+        MessageSelectedChoice(removeTodo);
+    }
+else if ((userChoice == "E")||(userChoice == "e"))
+    {
+        MessageSelectedChoice(exitStage);
+        Console.ReadKey();
+    }
+else
+    {
+        Console.WriteLine("Error");
+    }
+
+void MessageSelectedChoice (string selectedChoice = "Selected Choice: ")
+{
+    Console.WriteLine("Selected Choice: " + selectedChoice);
+}
